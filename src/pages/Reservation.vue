3e48f0166bc4bbba567373e771150d1c05467b9c
Kartik Riziya <kartikriziya30721@gmail.com>
2023-06-10 15:27:17 +0200

    worked on ratings
    colleted stars passed as emit and props

--- src/pages/Reservation.vue
+++ src/pages/Reservation.vue
@@ -15,11 +15,15 @@
             <h2>Olivewood restaurent</h2>
           </div>
           <div class="col-sm-4 col-md-12"></div>
-          <Rating />
+          <!-- Emit named '@getStars' called hier from 'Rating.vue' and
+             received Stars passes to a function name 'getStars'
+              as a Parameter in this same page Reservation.vue -->
+          <Rating @getStars="getStars" />
         </div>
       </div>
       <div class="col-md-6 col-lg-7 col-xl-8" id="tableLayout" style="background-color: brown">
-        <CustomerDetails />
+        <!-- passing 'sendStars' as a Prop name ':sendStars' to CustomerDetails.vue -->
+        <CustomerDetails :sendStars="sendStars" />
       </div>
     </div>
     <div class="row pt-5 pb-5">
@@ -37,6 +41,19 @@ import { ref } from 'vue'
 import axios from 'axios'
 import Rating from '../components/Rating.vue'
 import CustomerDetails from '../components/CustomerDetails.vue'
+
+const sendStars = ref('') // stars will be updated after getting 'getStars()' called in 'sendStars' variable as const
+
+/*********************************************************************************************************/
+/* Start of getStars(event) 
+/* -> getting Stars in a form of 'event' parameter from Rating.vue(Child) => as function name 'getStars()'
+/* -> will be saved in 'sendStars' variable as const
+/* -> will be passed as a Props to CustomerDetails.vue(Child) => as Prop name 'sendStars'
+/*********************************************************************************************************/
+function getStars(event) {
+  sendStars.value = event
+}
+/* End of getStars(event) */
 </script>
 
 <style scoped>
