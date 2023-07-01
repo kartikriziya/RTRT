4556aa5af26c98440ea36af33d394a71797910f4
Nancy Balar <nancybalar132313@gmail.com>
2023-07-01 18:59:31 +0200

    Cancel page updated

--- src/pages/Cancel.vue
+++ src/pages/Cancel.vue
@@ -27,25 +27,20 @@
                     {{ reservation.reservation_id }}
                     <input
                       type="text"
-                      :id="reservation.Id"
-                      :value="reservation.Id"
-                      @click="reservationAction($event.target.value)"
+                      :id="reservation.reservation_id"
+                      :for="reservation.reservation_id"
+                      :value="reservation.reservation_id"
+                      @click="cancelReservation($event.target.value)"
                       style="display: none"
                     />
                   </td>
                   <td id="table_row_data">{{ reservation.reserve_date }}</td>
                   <td id="table_row_data">{{ reservation.reserve_time }}</td>
-                  <td v-show="reservation.reservation_status != 0" id="table_row_data">
-                    <button class="btn" id="cancel_reservation_btn">Cancel</button>
-                  </td>
-                  <td
-                    v-show="reservation.reservation_status == 0"
-                    id="table_row_data"
-                    style="cursor: none"
-                  >
-                    Cancelled
-                  </td>
-                </tr>
+                  <td id="table_row_data">
+                  <button v-if="reservation.reservation_status != 0" class="btn" id="cancel_reservation_btn" @click="cancelReservation(reservation.reservation_id)">Cancel</button>
+                  <span v-else>Cancelled</span>
+                </td>
+              </tr>
               </tbody>
             </table>
           </div>
@@ -63,26 +58,43 @@ import axios from 'axios'
 const store = inject('store')
 const Base_Url = 'https://olivewood.elementfx.com'
 const action_id = ref('')
-const action = ref('')
+
+
+const LoggedIn_Email = ref('')
+LoggedIn_Email.value = JSON.parse(sessionStorage.getItem('user-email'))
 
 onMounted(() => {
   store.state.isLoading = false
   showReservations()
 })
 
-const reservationList = ref() // Array named 'reservationList' as const contains all Reservation of the selected Date.
+const reservationList = ref('') 
 
 async function showReservations() {
   let result = await axios.post(Base_Url + '/reservationAction.php', {
     action: 'user',
-    date: '2023-07-26'
+    loggedInEmail: LoggedIn_Email.value
   })
   if (result.status == 200 || result.status == 201) {
     reservationList.value = result.data
+    console.log(result.data)
   }
+
 }
 
-async function cancelReservation() {}
+async function cancelReservation(id) {
+  action_id.value = id
+  console.log('Reservation ID: ' + action_id.value + ' and has been cancelled.' )
+  let result = await axios.post(Base_Url + '/reservationAction.php', {
+    action: 'user',
+    actionID: action_id.value,
+    loggedInEmail: LoggedIn_Email.value
+  })
+  if (result.status == 200 || result.status == 201) {
+    console.log(result.data)
+    await showReservations();
+  }
+}
 </script>
 
 <style scoped>
